e8a4493377bc39faab9f6ca4d2089caabbc06935
temple.zhou <temple.zhou@foxmail.com>
2017-11-19 23:26:55 +0800

    zookeeper setup

--- setup_hadoop_env/readme.md
+++ setup_hadoop_env/readme.md
@@ -1,3 +1,5 @@
 ### 使用Docker配置hadoop 1+2集群
 
 > 以前年轻时都用VBox做自己的学习环境，如今尝试了下docker，感觉甚爽
+
+#### 本着步子迈小点防止扯到蛋的原则(也为了更好的给初学者学习)，docker镜像里只有jdk和hadoop，zookeeper、hive啥的后面都用ansible往上推
--- setup_hadoop_env/setup-network.sh
+++ setup_hadoop_env/setup-network.sh
@@ -6,3 +6,4 @@ ip addr add 192.168.2.1/24 dev br0
 pipework br0 hadoop0 192.168.2.10/24
 pipework br0 hadoop1 192.168.2.11/24
 pipework br0 hadoop2 192.168.2.12/24
+pipework br0 hadoop3 192.168.2.13/24
