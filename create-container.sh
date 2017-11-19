c334d698fa777f6a667074cb41f93419cf9e1a48
temple.zhou <temple.zhou@foxmail.com>
2017-11-19 13:36:33 +0800

    del file

--- create-container.sh
+++
@@ -1,5 +0,0 @@
-docker run --name hadoop0 --hostname hadoop0 -d -P -p 50070:50070 -p 8088:8088 temple/centos-hadoop
-
-docker run --name hadoop1 --hostname hadoop1 -d -P temple/centos-hadoop
-
-docker run --name hadoop2 --hostname hadoop2 -d -P temple/centos-hadoop
--- start-container.sh
+++
@@ -1,5 +0,0 @@
-docker start hadoop0
-docker start hadoop1
-docker start hadoop2
-
-./setup-network.sh
